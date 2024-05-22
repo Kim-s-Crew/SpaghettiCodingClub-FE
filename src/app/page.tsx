2e08a613d286e5c33738dc133816c46504e78840
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2024-05-22 18:40:04 +0900

    test: dnd 로직 구현 시도

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,44 @@
-import { Button } from '@nextui-org/react';
-import Link from 'next/link';
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       hello world
       <div>
-        <Button color='danger'>nextui button</Button>
-        <Button color='danger'>dev test</Button>
+        <Button color="danger">nextui button</Button>
+        <Button color="danger">dev test</Button>
         <p>임시 바로가기</p>
         <ul>
           <li>
-            <Link href={'/login'}>로그인</Link>
+            <Link href={"/login"}>로그인</Link>
           </li>
           <li>
-            <Link href={'/register'}>회원가입</Link>
+            <Link href={"/register"}>회원가입</Link>
           </li>
           <li>
-            <Link href={'/admin/student'}>수강생 관리</Link>
+            <Link href={"/admin/student"}>수강생 관리</Link>
           </li>
           <li>
-            <Link href={'/admin/student/userid'}>수강생 관리(개인)</Link>
+            <Link href={"/admin/student/userid"}>수강생 관리(개인)</Link>
           </li>
           <li>
-            <Link href={'/admin/track'}>트랙 관리</Link>
+            <Link href={"/admin/track"}>트랙 관리</Link>
           </li>
           <li>
-            <Link href={'/admin/trackweek'}>주차 관리</Link>
+            <Link href={"/admin/trackweek"}>주차 관리</Link>
           </li>
           <li>
-            <Link href={'/admin/notice'}>공지사항 관리</Link>
+            <Link href={"/admin/notice"}>공지사항 관리</Link>
           </li>
           <li>
-            <Link href={'/admin/track'}>트랙 관리 페이지</Link>
+            <Link href={"/admin/track"}>트랙 관리 페이지</Link>
           </li>
           <li>
-            <Link href={'/admin/schedule'}>일정 관리</Link>
+            <Link href={"/admin/schedule"}>일정 관리</Link>
+          </li>
+          <li>
+            <Link href={"/admin/teambuilding"}>팀빌딩</Link>
           </li>
         </ul>
       </div>
