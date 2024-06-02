13a081827f4c29ca6c69d1996c5372614089d735
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2024-06-02 22:27:32 +0900

    add : 사용자트랙변경 api 연결

--- src/apis/student.ts
+++ src/apis/student.ts
@@ -1,14 +1,40 @@
+import { changeTrackData } from '@/types/types';
 import axios from 'axios';
 
+const token = sessionStorage.getItem('token');
 // 트랙 참여자 전체 조회
 export const getStudents = async (trackId: number) => {
-  const token = sessionStorage.getItem('token');
   console.log(token);
   try {
     const response = await axios.get(
       `${process.env.NEXT_PUBLIC_SERVER_URL}/trackParticipants/${trackId}`,
       {
         headers: {
+          Authorization: `${token}`,
+        },
+      },
+    );
+    console.log(response);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// student 트랙 변경
+export const changeTrack = async ({
+  userId,
+  oldTrackId,
+  newTrackId,
+}: changeTrackData) => {
+  console.log(userId, oldTrackId);
+  try {
+    const response = await axios.put(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/trackParticipants/${userId}/${oldTrackId}`,
+      { newTrackId },
+      {
+        headers: {
+          'Content-Type': 'application/json', // Add Content-Type header
           Authorization: `${token}`,
         },
       },
